6108edb82904a96dd6d681ad067c1540be1d0a41
Yogesh Anil Waghmare <wyogesh703@gmail.com>
2025-10-15 09:46:45 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,15 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()  # <-- load .env before imports
+
 from app import create_app
+
+print("MONGO_URI =", os.getenv("MONGO_URI"))  # debug: must print your URI
 
 app = create_app()
 
 if __name__ == "__main__":
-  
     port = int(os.environ.get("PORT", 5000))
     debug_mode = os.environ.get("FLASK_DEBUG", "False").lower() == "true"
     app.run(host="0.0.0.0", port=port, debug=debug_mode)
